b640100e2c38868a932f31ee58a9e870588f2025
Aliaksandr Karpovich <alexsure.k@gmail.com>
2019-02-12 21:06:52 +0300

    Updating project structure and adding services as submodules

--- telegram-mcs/config.example.py
+++
@@ -1,2 +0,0 @@
-# -*- coding: utf-8 -*-
-token = 'telegram_bot_token'
--- ui-mcs/web/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import homePageView, telegramView
-
-urlpatterns = [
-    path('', homePageView, name='home'),
-    path('telegram', telegramView, name='telegram')
-]
